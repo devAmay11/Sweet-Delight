c0ec46692a750ab400e0e574811661cf250c58e8
devAmay11 <amayvaidya112@email.com>
2024-11-23 18:55:29 +0530

    mongoDB connectivity

--- sweet_delight/views.py
+++ sweet_delight/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .mongo_client import get_mongodb
+mongodbConn = get_mongodb()
 
-def home(request):
+def home(request):    
+    countData = list(mongodbConn.Countries.aggregate([
+        {"$match": {"id": 2}},
+        {"$project": {"_id": 0}}
+    ]))
+
+    print("-------countData",countData)
     return render(request, "index/index.html",{})
